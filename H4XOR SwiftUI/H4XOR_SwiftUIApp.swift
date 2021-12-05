c3096b8da2602c0ebfcc9d596f339dad028066d9
Raeein Bagheri <raeeinbagheri80@gmail.com>
2021-12-05 17:26:33 -0500

    Initial Commit

---
+++ H4XOR SwiftUI/H4XOR_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  H4XOR_SwiftUIApp.swift
+//  H4XOR SwiftUI
+//
+//  Created by Raeein Bagheri on 2021-12-05.
+//
+
+import SwiftUI
+
+@main
+struct H4XOR_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
